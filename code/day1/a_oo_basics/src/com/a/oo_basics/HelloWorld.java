494aa971a674471cee3f71066abc9f314d24a94a
rgupta00 <rgupta.mtech@gmail.com>
2024-07-22 11:58:13 +0530

    some chages

--- code/day1/a_oo_basics/src/com/a/oo_basics/HelloWorld.java
+++ code/day1/a_oo_basics/src/com/a/oo_basics/HelloWorld.java
@@ -2,7 +2,7 @@ package com.a.oo_basics;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("change is the only constant");
+		System.out.println("change is the only constant!!!");
 	}
 
 }
